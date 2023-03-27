fa8dbfffa458979734f259cac32109dfd46608ab
arialghifari <arialghifari99@gmail.com>
2023-03-27 09:21:17 +0800

    using plural posts

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server'
 const prisma = new PrismaClient()
 
 export async function GET(req: NextRequest) {
-  const post = await prisma.post.findMany()
+  const posts = await prisma.post.findMany()
 
-  return NextResponse.json(post)
+  return NextResponse.json(posts)
 }
